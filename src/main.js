0a21e3302197e4959bc6900705d40dcd5bba23ed
Faith Ncube <88321504+Faithmilly2021@users.noreply.github.com>
2023-12-23 23:05:33 +0200

    template breakdown

--- src/main.js
+++ src/main.js
@@ -1,7 +1,31 @@
 
 import { createApp } from 'vue';
-import App from './App.vue';
+import Layout from './Layout.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-createApp(App).mount('#app');
+
+import './assets/css/style.css';
+// import './assets/vendor/boxicons/css/boxicons.min.css';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import Profile from './components/Profile.vue';
+import Home from './components/Home.vue';
+
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/profile', component: Profile }
+
+  ];
+  
+  const router = createRouter({
+    history: createWebHistory(),
+    routes
+  });
+
+const app = createApp(Layout)
+app.use(router);
+app.mount('#app');
+
+
